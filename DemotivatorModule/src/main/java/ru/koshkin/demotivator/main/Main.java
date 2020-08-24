942a785cd616d03f883820b60c7351bda49a4dcc
VladKoshkin <xenomorph-balash@yandex.ru>
2020-08-24 18:29:18 +0300

    Делает демотиваторы на основе размера изображения

--- DemotivatorModule/src/main/java/ru/koshkin/demotivator/main/Main.java
+++ DemotivatorModule/src/main/java/ru/koshkin/demotivator/main/Main.java
@@ -1,15 +1,14 @@
 package ru.koshkin.demotivator.main;
 
-import javax.swing.*;
 import java.io.*;
 
-public class Main extends JFrame {
+public class Main {
 
     private static final String ORIG_FILE_PATH = "original/orig.jpg";
     private static final String RESULT_FILE_PATH = "result.jpg";
 
-    private static final String upperText = "Гигант мысли";
-    private static final String lowerText = "Его идеи будут актуальны всегда";
+    private static final String upperText = "Верхний текст";
+    private static final String lowerText = "Нижний текст";
 
     public static void main(String[] args) throws IOException {
         DemotivatorCreator creator = new DemotivatorCreator(ORIG_FILE_PATH, RESULT_FILE_PATH);
